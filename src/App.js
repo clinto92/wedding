5057ca558e88b7b0b45fd0afe3fc6ba8fc3ec057
Clinto_92_Abraham <clinto92@gmail.com>
2022-08-17 23:25:42 +0530

    Weds static photos (#4)
    * Added static photos
    
    * Added : CircleCI; build/ ; netlify : redirects
    
    Co-authored-by: Clinto Abraham <clinto.abraham@srijan.net>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 // import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
 import HighlightsPhotos from './Components/HighLightsPhotos';
-import DragNdrop  from './Components/DragNdrop';
-
+// import DrawerAppBar from './Components/AppBar';
 function App() {
-  const fileUploadProp = {
-    accept: 'image/*',
-    onChange: (event) => {
-        if (
-            event.target.files !== null &&
-            event.target?.files?.length > 0
-        ) {
-            console.log(`Saving ${event.target.value}`)
-        }
-    },
-    onDrop: (event) => {
-        console.log(`Drop ${event.dataTransfer.files[0].name}`)
-    },
-  }
   return (
-    <div className="App">
-      <DragNdrop {...fileUploadProp} />
+    <>
+    {/* <DrawerAppBar /> */}
       <HighlightsPhotos />
-    </div>
+    </>
   );
 }
 
--- src/Components/HighLightsPhotos/index.jsx
+++ src/Components/HighLightsPhotos/index.jsx
@@ -1,65 +1,114 @@
 import * as React from 'react';
-import { Box, Grid, Button } from '@mui/material';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import storage  from '../../firebase'
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Fade from '@mui/material/Fade';
 
-export default function HighlightsPhotos() {
-  const [images, setImages] = React.useState(null);
-  const fileInput = React.useRef();
+import Chippy from '../../Assets/Marriage/Chippy.JPG'
+import Abraham from '../../Assets/Marriage/Abraham.JPG'
+import Chindu from '../../Assets/Marriage/Chindu.JPG'
+import Chindu2 from '../../Assets/Marriage/Chindu2.JPG'
+import Chindu3 from '../../Assets/Marriage/Chindu3.JPG'
+import Chindu4 from '../../Assets/Marriage/Chindu4.JPG'
+import Chippy2 from '../../Assets/Marriage/Chippy2.JPG'
+import Chippy3 from '../../Assets/Marriage/Chippy3.JPG'
+import Chippy4 from '../../Assets/Marriage/Chippy4.JPG'
+import Chippy5 from '../../Assets/Marriage/Chippy5.JPG'
+import Chippy7 from '../../Assets/Marriage/Chippy7.JPG'
+import Chippy8 from '../../Assets/Marriage/Chippy8.JPG'
+import Chippy9 from '../../Assets/Marriage/Chippy9.JPG'
+import Chippy10 from '../../Assets/Marriage/Chippy10.JPG'
+import ChippyCicily from '../../Assets/Marriage/ChippyCicily.JPG'
+import ChippyFamily from '../../Assets/Marriage/ChippyFamily.JPG'
+import Cicily from '../../Assets/Marriage/Cicily.JPG'
+import Clinto from '../../Assets/Marriage/Clinto.JPG'
+import Clinto2 from '../../Assets/Marriage/Clinto2.JPG'
+import Clinto3 from '../../Assets/Marriage/Clinto3.JPG'
+import Clinto4 from '../../Assets/Marriage/Clinto4.JPG'
+import ClintoChindu from '../../Assets/Marriage/ClintoChindu.JPG'
+import ClintoChindu2 from '../../Assets/Marriage/ClintoChindu2.JPG'
+import ClintoChippy from '../../Assets/Marriage/ClintoChippy.JPG'
+import ClintoChippy2 from '../../Assets/Marriage/ClintoChippy2.JPG'
+import Family from '../../Assets/Marriage/Family.JPG'
+import Mini from '../../Assets/Marriage/Mini.JPG'
+import MinuSneha from '../../Assets/Marriage/MinuSneha.JPG'
+import ShinyTommy from '../../Assets/Marriage/ShinyTommy.JPG'
+import Threesama from '../../Assets/Marriage/Threesama.JPG'
 
-  const handleChange = (e) => {
-    fileInput.current.click()
-    console.log(e.target.value, e.target, e.target.file)
-    if(e.target.value){
-      setImages(e.target.value)
+
+
+
+function ScrollTop(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
     }
-    // fileInput.current.click()
-  }
-  const handleUpload = (e) => {
-    console.log(e.target, images)
-    const uploadTask = storage.ref(`files/${images.name}`).put(images);
-    uploadTask.on(
-      "state_changed",
-      snapshot => {},
-      error => {
-        console.log(error)
-      },
-      () => {
-        storage
-          .ref("files")
-          .child(images.name)
-          .getDownloadURL()
-          .then((url) => {
-            console.log(url)
-          })
-      }
-    )
-  }
-  return (
-    <Box sx={{ width: 1200, height: 1450, overflowY: 'scroll', padding: '100px', margin: '20px' }}>
-      <Grid container>
-        <Grid item xs={6}>
-          <input type="file" onChange={handleChange}  ref={fileInput} value={images} />
-        <Button variant={'text'} onClick={handleUpload}> <CloudUploadIcon /> Upload Image</Button>
-        </Grid>
-        <Grid item xs={6}>
-        <Button 
-        variant="contained" 
-        color="primary" 
-        onClick={handleChange}
-      >
-         <CloudUploadIcon />
-      </Button>
+  };
 
-      <input 
-        ref={fileInput} 
-        type="file" 
-        style={{ display: 'none' }} 
-      />
-        </Grid>
-      </Grid>
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function BackToTop(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h3" component="div">
+            {`Clinto & Chippy`}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+      <Container>
+      <Box sx={{ width: 1200, height: 1450, overflowY: 'scroll', padding: '100px', margin: '20px' }}>
+      <Typography variant="h3">{`Wedding`}</Typography>
+     
+     
       <ImageList variant="masonry" cols={3} gap={8}>
         {itemData.map((item) => (
           <ImageListItem key={item.img} onClick={() => console.log("clicked image list", item.img, item.title)}>
@@ -73,56 +122,137 @@ export default function HighlightsPhotos() {
         ))}
       </ImageList>
     </Box>
+      </Container>
+      <ScrollTop {...props}>
+        <Fab size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </React.Fragment>
   );
 }
 
+
+
 const itemData = [
   {
-    img: 'https://firebasestorage.googleapis.com/v0/b/wedding-clinto-chippy.appspot.com/o/4C2A%20(58).jpg?alt=media&token=18e45ce7-a56b-45b5-b195-994f31460c58',
-    title: 'Bed',
+    img: Chippy,
+    title: 'chippy',
   },
   {
-    img: 'https://drive.google.com/file/d/1sDKJQhDijjHDmIT2pr8Mr7EUdOuWHIIv/view?usp=sharing',
-    title: 'Books',
+    img: Clinto,
+    title: 'Clinto',
   },
   {
-    img: 'gs://wedding-clinto-chippy.appspot.com/4C2A (58).jpg',
-    title: 'Sink',
+    img: Chippy2,
+    title: 'Chippy',
   },
   {
-    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-    title: 'Kitchen',
+    img: Clinto4,
+    title: 'Clinto',
   },
   {
-    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-    title: 'Blinds',
+    img: Chippy3,
+    title: 'Chippy',
   },
   {
-    img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-    title: 'Chairs',
+    img: Clinto3,
+    title: 'Clinto',
   },
   {
-    img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-    title: 'Laptop',
+    img: ChippyFamily,
+    title: 'Chippy & Family',
   },
   {
-    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-    title: 'Doors',
+    img: Abraham,
+    title: 'Abraham',
   },
   {
-    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-    title: 'Coffee',
+    img: Mini,
+    title: 'Mini',
   },
   {
-    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-    title: 'Storage',
+    img: Threesama,
+    title: 'Threesama',
   },
   {
-    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-    title: 'Candle',
+    img: ShinyTommy,
+    title: 'Shiny Tommy',
   },
   {
-    img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-    title: 'Coffee table',
+    img: ClintoChindu,
+    title: 'Clinto & Chindu',
+  },
+  {
+    img: ClintoChindu2,
+    title: 'Clinto & Chindu',
+  },
+  {
+    img: ChippyCicily,
+    title: 'Chippy & Cicily',
+  },
+  {
+    img: ClintoChippy,
+    title: 'Chippy & Clinto',
+  },
+  {
+    img: ClintoChippy2,
+    title: 'Chippy & Clinto',
+  },
+  {
+    img: MinuSneha,
+    title: 'Minu & Sneha',
+  },
+  {
+    img: Cicily,
+    title: 'Cicily',
+  },
+  {
+    img: Chindu,
+    title: 'Chindu',
+  },
+  {
+    img: Chippy7,
+    title: 'Chippy',
+  },
+  {
+    img: Chippy4,
+    title: 'Chippy',
+  },
+  {
+    img: Chippy5,
+    title: 'Chippy',
+  },
+  {
+    img: Chippy8,
+    title: 'Chippy',
+  },
+  {
+    img: Chippy9,
+    title: 'Chippy',
+  },
+  {
+    img: Chippy10,
+    title: 'Chippy',
+  },
+  {
+    img: Clinto2,
+    title: 'Clinto',
+  },
+  {
+    img: Family,
+    title: 'Family',
+  },
+  {
+    img: Chindu2,
+    title: 'Chippy',
+  },
+  {
+    img: Chindu3,
+    title: 'Chippy',
+  },
+  {
+    img: Chindu4,
+    title: 'Chippy',
   },
 ];
